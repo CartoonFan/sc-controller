f22d7aec8d9cbc461c15b685c4bf2044fba54575
Restyled.io <commits@restyled.io>
2020-12-09 04:00:33 +0000

    Restyled by black

--- tests/test_strings/test_modifiers.py
+++ tests/test_strings/test_modifiers.py
@@ -1,4 +1,3 @@
-
 from . import _parses_as
 from scc.actions import AxisAction
 from scc.modifiers import *
@@ -16,7 +15,7 @@ class TestModifiers(object):
         """
         # All options
         if not _parses_as(
-                "ball(15, 40, 15, 0.1, 3265, 4, axis(ABS_X))",
-                BallModifier(15, 40, 15, 0.1, 3265, 4, AxisAction(Axes.ABS_X)),
+            "ball(15, 40, 15, 0.1, 3265, 4, axis(ABS_X))",
+            BallModifier(15, 40, 15, 0.1, 3265, 4, AxisAction(Axes.ABS_X)),
         ):
             raise AssertionError
