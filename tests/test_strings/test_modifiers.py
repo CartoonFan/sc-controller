40aa37b241594fa01d64b207e5e561ac7560610d
Restyled.io <commits@restyled.io>
2020-12-09 00:39:22 +0000

    Restyled by autopep8

--- tests/test_strings/test_modifiers.py
+++ tests/test_strings/test_modifiers.py
@@ -4,17 +4,18 @@ from scc.modifiers import *
 from . import _parses_as
 import inspect
 
+
 class TestModifiers(object):
-	
-	# TODO: Much more tests
-	# TODO: test_tests
-	
-	def test_ball(self):
-		"""
-		Tests if BallModifier can be converted from string
-		"""
-		# All options
-		assert _parses_as(
-			"ball(15, 40, 15, 0.1, 3265, 4, axis(ABS_X))",
-			BallModifier(15, 40, 15, 0.1, 3265, 4, AxisAction(Axes.ABS_X))
-		)
+
+    # TODO: Much more tests
+    # TODO: test_tests
+
+    def test_ball(self):
+        """
+        Tests if BallModifier can be converted from string
+        """
+        # All options
+        assert _parses_as(
+            "ball(15, 40, 15, 0.1, 3265, 4, axis(ABS_X))",
+            BallModifier(15, 40, 15, 0.1, 3265, 4, AxisAction(Axes.ABS_X))
+        )
